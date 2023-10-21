a2fea3040912d1e5b35350616041e5b8009d2337
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2023-10-21 18:01:00 +0300

    remove old code

--- src/modules/auth/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
-export class LoginDto {
-  @IsNotEmpty()
-  @IsEmail()
-  readonly email: string;
-  @IsNotEmpty()
-  readonly password: string;
-}
